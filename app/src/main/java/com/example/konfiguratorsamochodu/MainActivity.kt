c9b9371631c9a9ce44248bbb3a614f824b3cf313
Damian <damiankontakt7@gmail.com>
2025-03-22 19:10:09 +0100

    Zaimplementowano funkcjonalność: podsumowanie zamówienia

--- app/src/main/java/com/example/konfiguratorsamochodu/MainActivity.kt
+++ app/src/main/java/com/example/konfiguratorsamochodu/MainActivity.kt
@@ -20,5 +20,44 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val imageView = findViewById<ImageView>(R.id.imageViewAuto)
+        val radioGroup = findViewById<RadioGroup>(R.id.rodzajNadwoziaRG)
+        val zamowButton = findViewById<Button>(R.id.zamowButton)
+        val checkKlima = findViewById<CheckBox>(R.id.checkKlima)
+        val checkSiedzenia = findViewById<CheckBox>(R.id.checkSiedzenia)
+        val podsumowanie = findViewById<TextView>(R.id.podsumowanie)
+
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            val imageResource = when (checkedId) {
+                R.id.sedan -> R.drawable.sedan
+                R.id.suv -> R.drawable.suv
+                R.id.hatchback -> R.drawable.hatchback
+                else -> R.drawable.sedan
+            }
+            imageView.setImageResource(imageResource)
+        }
+
+        zamowButton.setOnClickListener {
+            val selectedCarId = radioGroup.checkedRadioButtonId
+            val selectedCar = when (selectedCarId) {
+                R.id.sedan -> "Sedan"
+                R.id.suv -> "Suv"
+                R.id.hatchback -> "Hatchback"
+                else -> "Nie wybrano typu nadwozia"
+            }
+
+
+            val dodatkoweWyp = mutableListOf<String>()
+            if (checkKlima.isChecked) dodatkoweWyp.add("Klimatyzacja")
+            if (checkSiedzenia.isChecked) dodatkoweWyp.add("Skórzane siedzenia")
+
+            val dodatkoweWypText = if (dodatkoweWyp.isNotEmpty()) dodatkoweWyp.joinToString(", ") else "Brak dodatkowego wyposażenia"
+
+            val zamowienie = "Zamówienie:\nRodzaj nadwozia: $selectedCar\nDodatkowe wyposażenie: $dodatkoweWypText"
+
+            podsumowanie.text = zamowienie
+        }
     }
 }
